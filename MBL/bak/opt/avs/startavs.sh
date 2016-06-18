fed37c390716659c807e39b0663206364f28d4c4
杨祜 <yangguo.gcy@alibaba-inc.com>
2016-06-18 17:48:28 +0800

    Remove bak

--- MBL/bak/opt/avs/startavs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export LC_ALL=zh_CN.UTF-8
-#
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/bin:/opt/sbin:.
-export LANG=zh_CN.UTF-8
-cd /opt/avs
-/bin/rm -f /shares/Media/.avs/*.ts
-LD_LIBRARY_PATH=/opt/avs/lib
-python avs.py &
