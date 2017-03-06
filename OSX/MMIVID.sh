1fa7df270297759696a57e67a60b246ddaf18315
Yonsm <I@YONSM.NET>
2017-03-06 14:00:16 +0800

    For mp4 extension

--- OSX/MMIVID.sh
+++ OSX/MMIVID.sh
@@ -9,7 +9,7 @@ MakeMMIViD()
 	FILE="${1##*/}"
 	NAME="${FILE%.*}"
 	DVID="$DDIR/$NAME.mp4"
-	ffmpeg -i "$1" -y -strict -2 -acodec aac -ab 225k -ar 44.1k -ac 2 -vf crop="in_w:in_w*338/720" -s 720x338 -vcodec libx264 -crf 16 -profile:v main -level 3.1 "$DVID"
+	ffmpeg -i "$1" -strict -2 -acodec aac -ab 225k -ar 44.1k -ac 2 -vf crop="in_w:in_w*338/720" -s 720x338 -vcodec libx264 -crf 16 -profile:v main -level 3.1 "$DVID"
 }
 
 VDIR=$1
@@ -22,7 +22,3 @@ if [ -f "$VDIR" ]; then
 else
 	find -E "$VDIR" -iregex ".*\.(mp4|mkv|mov|avi)" -maxdepth 1 | while read f ; do MakeMMIViD "$f" ; done
 fi
-
-
-
-#-exec echo ffmpeg -i "{}" -y -strict -2 -acodec aac -ab 225k -ar 44.1k -ac 2 -vf crop="in_w:in_w*338/720" -s 720x338 -vcodec libx264 -crf 16 -profile:v main -level 3.1 MMIVID/`basename {} .mp4` \;
