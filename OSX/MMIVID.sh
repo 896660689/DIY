5bbbbdd630dd1a98710296b9105c8d984b74bbc9
Yonsm <I@YONSM.NET>
2017-03-06 14:08:53 +0800

    from null

--- OSX/MMIVID.sh
+++ OSX/MMIVID.sh
@@ -9,7 +9,7 @@ MakeMMIViD()
 	FILE="${1##*/}"
 	NAME="${FILE%.*}"
 	DVID="$DDIR/$NAME.mp4"
-	ffmpeg -i "$1" -strict -2 -acodec aac -ab 225k -ar 44.1k -ac 2 -vf crop="in_w:in_w*338/720" -s 720x338 -vcodec libx264 -crf 16 -profile:v main -level 3.1 "$DVID"
+	ffmpeg -i "$1" -strict -2 -acodec aac -ab 225k -ar 44.1k -ac 2 -vf crop="in_w:in_w*338/720" -s 720x338 -vcodec libx264 -crf 17 -profile:v main -level 3.1 "$DVID"</dev/null
 }
 
 VDIR=$1
