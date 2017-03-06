bbe280bab531b8abeea105d0cb3105bfe5645fdd
Yonsm <I@YONSM.NET>
2017-03-06 17:58:54 +0800

    支持字幕

--- OSX/MMIVID.sh
+++ OSX/MMIVID.sh
@@ -6,10 +6,22 @@ MakeMMIViD()
 	if [ ! -d "$DDIR" ]; then
 		mkdir "$DDIR"
 	fi
+	
 	FILE="${1##*/}"
 	NAME="${FILE%.*}"
 	DVID="$DDIR/$NAME.mp4"
-	ffmpeg -i "$1" -strict -2 -acodec aac -ab 225k -ar 44.1k -ac 2 -vf crop="in_w:in_w*338/720" -s 720x338 -vcodec libx264 -crf 17 -profile:v main -level 3.1 "$DVID"</dev/null
+
+	FOPT="-y -strict -2 -acodec aac -ab 225k -ar 44.1k -ac 2 -vf crop=in_w:in_w*338/720 -s 720x338 -vcodec libx264 -crf 17 -profile:v main -level 3.1"
+	
+	SUBT="${1%.*}.srt"
+	if [ ! -f "$SUBT" ]; then
+		SUBT="${1%.*}.ass"
+	fi
+	if [ -f "$SUBT" ]; then
+		ffmpeg -i "$1" $FOPT -vf subtitles="$SUBT":original_size=720x338 "$DVID"</dev/null
+	else
+		ffmpeg -i "$1" $FOPT "$DVID"</dev/null
+	fi
 }
 
 VDIR=$1
