56c9f6138e11202c3cfea2ceaf7f7a44c2a7fa3d
Yonsm <I@YONSM.NET>
2017-03-06 11:08:48 +0800

    MMIVID

--- OSX/MMI.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ ! -d OUT ]; then
-	mkdir OUT
-fi
-find -E . -iregex ".*\.(mp4|mkv|mov|avi)" -exec ffmpeg -i "{}" -strict -2 -acodec aac -ab 225k -ar 44.1k -ac 2 -vf crop="in_w:in_w*338/720" -s 720x338 -vcodec libx264 -crf 17 -profile:v main -level 3.1 "OUT/{}" \;
