88201c37edb70295096abb4eb1833ed31696f87e
Yonsm <I@YONSM.NET>
2017-03-07 14:27:10 +0800

    refine

--- WIN/MMIVID/MMIVID.cmd
+++ WIN/MMIVID/MMIVID.cmd
@@ -1,22 +1,23 @@
 @ECHO OFF
+PATH=%PATH%;%~dp0
 
-IF "%1"=="" (
+IF [%1]==[] (
 	SET VDIR=.
 ) ELSE (
 	SET VDIR=%1
 )
 
-IF EXIST "%VDIR%\." (
-	FOR %%A IN ("%VDIR%\*.mp4" *.mkv *.mov *.avi *.wmv *.vob) DO CALL :MakeMMIViD "%%A"
+IF EXIST %VDIR%\NUL (
+	FOR %%A IN (%VDIR%\*.mp4 *.mkv *.mov *.avi *.wmv *.vob) DO CALL :MakeMMIViD "%%A"
 ) ELSE (
-	CALL MakeMMIViD "%VDIR%"
+	CALL :MakeMMIViD %VDIR%
 )
 EXIT /b 0
 
 :MakeMMIViD
 	CD %~dp1
 	SET DDIR=OUT
-	IF NOT EXIST "%DDIR%" MD "%DDIR%"
+	IF NOT EXIST %DDIR% MD %DDIR%
 
 	SET DVID=%DDIR%\%~n1.mp4
 
