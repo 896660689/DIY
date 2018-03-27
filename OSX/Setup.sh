0ef0f018f6f05c4f9a7f0f0714c2c413c19fcf59
Yonsm <I@YONSM.NET>
2018-03-27 09:19:01 +0800

    Refine OSX

--- OSX/Setup.sh
+++ OSX/Setup.sh
@@ -3,9 +3,9 @@ CDIR=$(cd "${0%/*}"; pwd)
 cd $CDIR
 
 sudo mkdir -p /usr/local/bin/
-sudo ln -sf "$CDIR/BestVideo.sh" /usr/local/bin/bestvideo
-sudo ln -sf "$CDIR/RenImg.sh" /usr/local/bin/renimg
-sudo ln -sf "$CDIR/LinkApp.sh" /usr/local/bin/linkapp
+sudo ln -sf "$CDIR/usr/local/bin/bestvideo" /usr/local/bin/
+sudo ln -sf "$CDIR/usr/local/bin/renimg" /usr/local/bin/
+sudo ln -sf "$CDIR/usr/local/bin/linkapp" /usr/local/bin/
 sudo ln -sf "$CDIR/usr/local/bin/jhead" /usr/local/bin/
 sudo ln -sf "$CDIR/usr/local/bin/ffmpeg" /usr/local/bin/
 sudo ln -sf "$CDIR/usr/local/bin/adb" /usr/local/bin/
@@ -16,9 +16,11 @@ sudo ln -sf "$CDIR/usr/local/bin/jq" /usr/local/bin/
 sudo cp etc/hosts /etc/hosts
 
 sudo spctl --master-disable
-defaults write com.apple.desktopservices DSDontWriteNetworkStores true
-#defaults write com.apple.iTunes DeviceBackupsDisabled -bool YES
-defaults write com.apple.iTunes AutomaticDeviceBackupsDisabled -bool true
 
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+defaults write com.apple.iTunes AutomaticDeviceBackupsDisabled -bool true
+#defaults write com.apple.iTunes DeviceBackupsDisabled -bool YES
 
 # Chrome + Baidu (com.google.Chrome.mobileconfig): https://github.com/acgotaku/BaiduExporter
+open ./Chrome.mobileconfig
+./RamDisk.sh
