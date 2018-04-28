e411e4baa34c25778836cef3a4b739d443910391
Yonsm <I@YONSM.NET>
2018-04-28 15:34:39 +0800

    Setup script

--- HASS/Setup.sh
+++ HASS/Setup.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
-# https://blog.agchapman.com/using-qemu-to-emulate-a-raspberry-pi/
-# https://github.com/dhruvvyas90/qemu-rpi-kernel
-# http://www.cnblogs.com/chengchen/p/6751420.html
-# http://tuntaposx.sourceforge.net/
-# http://drupal.bitfunnel.net/drupal/macosx-bridge-qemu
-
-sudo qemu-system-arm \
--kernel ./kernel-qemu-4.4.34-jessie \
--append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
--hda ~/Downloads/raspbian-stretch-lite.qcow \
--cpu arm1176 -m 256 \
--M versatilepb \
--serial stdio \
--net nic -net user \
--net tap,ifname=tap0,script=no,downscript=no
-
 sudo passwd root
 sudo passwd --unlock root
+nano /etc/ssh/sshd_config #PermitRootLogin yes
 
-#-net nic,model=virtio,macaddr=54:54:00:55:55:55 \
-#-net tap,script=./tap-up.sh,downscript=./tap-down.sh
+#root
+usermod -l admin pi
+groupmod -n admin pi
+mv /home/pi /home/admin
+usermod -d /home/admin admin
+nano /etc/sudoer #admin ALL=(ALL) NOPASSWD: ALL
+
+# Home Assistant
+apt-get update
+apt-get upgrade -y
+
+apt-get install python3 python3-pip
+apt-get install libavahi-compat-libdnssd-dev # homekit
+apt-get install mosquitto mosquitto-clients
+
+pip3 install homeassistant
+hass
+
