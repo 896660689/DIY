165ccc69a0b1d2ee9a438c697f923fb393c828ae
Yonsm <I@YONSM.NET>
2018-04-28 16:08:28 +0800

    Home Assistant setup

--- HASS/Setup.sh
+++ HASS/Setup.sh
@@ -11,14 +11,50 @@ mv /home/pi /home/admin
 usermod -d /home/admin admin
 nano /etc/sudoer #admin ALL=(ALL) NOPASSWD: ALL
 
-# Home Assistant
+#
 apt-get update
 apt-get upgrade -y
 
+# Home Assistant
 apt-get install python3 python3-pip
-apt-get install libavahi-compat-libdnssd-dev # homekit
-apt-get install mosquitto mosquitto-clients
-
 pip3 install homeassistant
+
+# HomeKit
+apt-get install libavahi-compat-libdnssd-dev
+
+# Mosquitto
+apt-get install mosquitto mosquitto-clients
+#cat /etc/mosquitto/mosquitto.conf #allow_anonymous true
+#systemctl stop mosquitto
+#sleep 2
+#rm -rf /var/lib/mosquitto/mosquitto.db
+#systemctl start mosquitto
+#sleep 2
+#mosquitto_sub -v -t '#'
+
+# Auto start
+cat <<EOF > /etc/systemd/system/homeassistant.service
+[Unit]
+Description=Home Assistant
+After=network-online.target
+
+[Service]
+Type=simple
+User=root
+ExecStart=/usr/local/bin/hass
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+# Debug
 hass
 
+# Restart
+echo .> /var/log/daemon.log; echo .>~/.homeassistant/home-assistant.log; systemctl restart homeassistant; tail -f /var/log/daemon.log
+
+# Upgrage
+systemctl stop homeassistant
+pip3 install --upgrade homeassistant
+systemctl start homeassistant
