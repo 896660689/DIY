94a321bb691fba89ba4a7752155e571aa67b302a
Yonsm <I@YONSM.NET>
2018-04-30 21:58:11 +0800

    Iperf

---
+++ ROUTER/Setup.sh
@@ -0,0 +1,21 @@
+
+mkdir /jffs/opt
+ln -nsf /jffs/opt /tmp/opt
+wget -O - http://pkg.entware.net/binaries/armv7/installer/entware_install.sh | /bin/sh
+
+echo "#!/bin/sh" > /jffs/scripts/services-start
+echo "sleep 20" >> /jffs/scripts/services-start
+echo "ln -ns /jffs/opt /tmp/opt" >> /jffs/scripts/services-start
+echo "/opt/etc/init.d/rc.unslung start" >> /jffs/scripts/services-start
+echo "#!/bin/sh" > /jffs/scripts/services-stop
+echo "/opt/etc/init.d/rc.unslung stop" >> /jffs/scripts/services-stop
+chmod a+rx /jffs/scripts/*
+
+opkg update
+opkg list
+#opkg install
+opkg remove
+
+
+#~/.ssh
+wget -O speedtest.py https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py; chmod +x speedtest.py; ./speedtest.py
