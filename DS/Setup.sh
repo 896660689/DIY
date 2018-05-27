f34bc7be5e666f1c02c3c3fc0cdf553e1ab6cbe0
Yonsm <I@YONSM.NET>
2018-05-27 11:40:40 +0800

    aria2c

--- DS/Setup.sh
+++ DS/Setup.sh
@@ -4,3 +4,7 @@
 
 #~/.ssh
 wget -O speedtest https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py; chmod +x speedtest.py; ./speedtest
+
+#Aria Deamon
+# mv /opt /volume1/Downloads/.aria
+# /volume1/Downloads/.aria/etc/init.d/S90aria
