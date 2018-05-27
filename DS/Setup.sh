2ea95e4f966f2e86dbf5facaa0c0d6aa08ecb37d
Yonsm <I@YONSM.NET>
2018-05-27 11:45:22 +0800

    ds

--- DS/Setup.sh
+++ DS/Setup.sh
@@ -1,10 +1,12 @@
 
+#Aria Deamon
+# mv /opt /volume1/Downloads/.opt
+# ln -s /volume1/Downloads/.opt /opt
+# /volume1/Downloads/.opt/etc/init.d/S90aria
+
 /opt/bin/busybox passwd admin
 /opt/bin/busybox passwd root
 
 #~/.ssh
+cd mv /opt/bin
 wget -O speedtest https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py; chmod +x speedtest.py; ./speedtest
-
-#Aria Deamon
-# mv /opt /volume1/Downloads/.aria
-# /volume1/Downloads/.aria/etc/init.d/S90aria
