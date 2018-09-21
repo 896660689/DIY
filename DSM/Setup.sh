820afa200839b8cdc9df05b2de47b5ca26f259fd
Yonsm <I@YONSM.NET>
2018-09-21 12:20:40 +0800

    ls

--- DSM/Setup.sh
+++ DSM/Setup.sh
@@ -10,3 +10,12 @@
 #~/.ssh
 cd mv /opt/bin
 wget -O speedtest https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py; chmod +x speedtest.py; ./speedtest
+
+cat <<\EOF > ~/.bashrc
+#!/bin/sh
+LS_OPTIONS=-la
+PATH=$PATH:/volume1/Downloads/.opt/bin/
+alias ls='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -l'
+alias l='ls $LS_OPTIONS -lA'
+EOF
