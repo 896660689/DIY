90d9182c63fd0d5f3e17f629d72dd93510e73405
Yonsm <I@YONSM.NET>
2018-11-14 21:33:29 +0800

    refine

--- DSM/Setup.sh
+++ DSM/Setup.sh
@@ -1,21 +1,20 @@
 
 #Aria Deamon
-# mv /opt /volume1/Downloads/.opt
-# ln -s /volume1/Downloads/.opt /opt
+ln -s /volume1/Downloads/.opt /opt
 # /volume1/Downloads/.opt/etc/init.d/S90aria
 
-/opt/bin/busybox passwd admin
-/opt/bin/busybox passwd root
-
 #~/.ssh
-cd mv /opt/bin
+cd /opt/bin
 wget -O speedtest https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py; chmod +x speedtest.py; ./speedtest
 
 cat << \EOF > ~/.bashrc
 #!/bin/sh
 LS_OPTIONS=-la
-PATH=$PATH:/volume1/Downloads/.opt/bin/
+PATH=$PATH:/volume1/Downloads/.opt/bin
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
 alias l='ls $LS_OPTIONS -lA'
 EOF
+
+/opt/bin/busybox passwd admin
+/opt/bin/busybox passwd root
---
+++ DSM/opt/bin/iperf
@@ -0,0 +1,3 @@
+#!/bin/sh
+CDIR=$(cd "${0%/*}"; pwd)
+LD_LIBRARY_PATH=$CDIR $CDIR/iperf3 -s
