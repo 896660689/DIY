aabbe639b208f1ea3d59d83ae3b4c0f350dbb3b3
Yonsm <Yonsm@qq.com>
2018-11-16 12:18:41 +0800

    opkg list

--- ROUTER/Setup.sh
+++ ROUTER/Setup.sh
@@ -24,3 +24,18 @@ ssh admin@router "mkdir /jffs/bin"
 scp iperf3 admin@router:/jffs/bin
 scp tcpdump admin@router:/jffs/bin
 #/jffs/bin/tcpdump -i br0 host 114.141.173.62 -w /tmp/tcpdump.cap
+
+#Entware-Ng
+mkdir /jffs/entware-ng.arm
+ln -nsf /jffs /tmp/opt
+wget -O - http://pkg.entware.net/binaries/armv7/installer/entware_install.sh | /bin/sh
+
+echo "#!/bin/sh" > /jffs/scripts/services-start
+echo "sleep 20" >> /jffs/scripts/services-start
+echo "ln -ns /jffs /tmp/opt" >> /jffs/scripts/services-start
+echo "/opt/etc/init.d/rc.unslung start" >> /jffs/scripts/services-start
+echo "#!/bin/sh" > /jffs/scripts/services-stop
+echo "/opt/etc/init.d/rc.unslung stop" >> /jffs/scripts/services-stop
+chmod a+rx /jffs/scripts/*
+
+opkg list
